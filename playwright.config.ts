9897b59f559aa5699ec687f2efd55d609abfb4ca
hendisantika <hendisantika@yahoo.co.id>
2024-07-28 23:42:15 +0700

    Add required libs 🫸🌀✏️📗🐧🐳

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import {defineConfig, devices} from '@playwright/test';
+import {testPlanFilter} from "allure-playwright/dist/types/testplan";
 
 /**
  * Read environment variables from file.
@@ -19,9 +20,11 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.CI ? 2 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    grep: testPlanFilter(),
+    reporter: [["line"], ["allure-playwright"]],
+    // reporter: 'allure-playwright',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
@@ -47,6 +50,11 @@ export default defineConfig({
             name: 'webkit',
             use: {...devices['Desktop Safari']},
         },
+
+        {
+            name: 'myProject',
+            use: {...devices['Desktop Chrome']},
+        }
 
         /* Test against mobile viewports. */
         // {
